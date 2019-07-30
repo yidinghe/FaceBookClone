de91d96c5796edff07ccbe263bca92fe0e86735d
Yiding He <yidinghe2012@gmail.com>
2019-07-29 21:30:10 -0400

    Initial Commit

---
+++ FaceBookClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceBookClone
+//
+//  Created by Yiding He on 7/29/19.
+//  Copyright © 2019 Yiding He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
