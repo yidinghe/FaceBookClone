a92c9be7d4d6e3435751b67c5c2034853c68ca3a
Yiding He <yidinghe2012@gmail.com>
2019-08-01 20:27:59 -0400

    Finished draft of login UI.

--- FaceBookClone/LoginVC.swift
+++ FaceBookClone/LoginVC.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
-    @IBOutlet weak var textFieldsView: UIView!
+    @IBOutlet var textFieldsView: UIView!
+    
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var registerButton: UIButton!
+    
+    @IBOutlet var leftLineView: UIView!
+    @IBOutlet var rightLineView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        configure_registerBtn()
         configure_textFieldsView()
+        configure_loginBtn()
+        configure_orLabel()
     }
     
     func configure_textFieldsView() {
@@ -41,5 +50,41 @@ class LoginVC: UIViewController {
         
         textFieldsView.layer.cornerRadius = 5
         textFieldsView.layer.masksToBounds = true
+    }
+    
+    func configure_loginBtn() {
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.masksToBounds = true
+        loginButton.isEnabled = false
+    }
+    
+    func configure_orLabel() {
+        let width = CGFloat(2)
+        let color = UIColor.groupTableViewBackground.cgColor
+        
+        let leftLine = CALayer()
+        leftLine.borderWidth = width
+        leftLine.borderColor = color
+        leftLine.frame = CGRect(x: 0, y: leftLineView.frame.height / 2 - width, width: leftLineView.frame.width, height: width)
+        
+        let rightLine = CALayer()
+        rightLine.borderWidth = width
+        rightLine.borderColor = color
+        rightLine.frame = CGRect(x: 0, y: rightLineView.frame.height / 2 - width, width: rightLineView.frame.width, height: width)
+        
+        leftLineView.layer.addSublayer(leftLine)
+        rightLineView.layer.addSublayer(rightLine)
+    }
+    
+    func configure_registerBtn() {
+        let border = CALayer()
+        border.borderColor = UIColor(red: 68/255, green: 105 / 255, blue: 176 / 255, alpha: 1).cgColor
+        border.borderWidth = 2
+        border.frame = CGRect(x: 0, y: 0, width: registerButton.frame.width, height: registerButton.frame.height)
+        
+        registerButton.layer.addSublayer(border)
+        
+        registerButton.layer.cornerRadius = 5
+        registerButton.layer.masksToBounds = true
     }
 }
