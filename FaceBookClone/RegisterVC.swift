1cf05b1b8003b442bc33093779a58cf85667d586
Yiding He <yidinghe2012@gmail.com>
2019-08-29 19:53:00 -0400

    Added Register VC.

---
+++ FaceBookClone/RegisterVC.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterVC.swift
+//  FaceBookClone
+//
+//  Created by Yiding He on 8/29/19.
+//  Copyright © 2019 Yiding He. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
